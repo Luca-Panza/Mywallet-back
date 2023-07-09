09a72c4b6021c6bc77810a03c7d7ab57ffc203d1
Luca-Panza <luca.panza.007@gmail.com>
2023-07-09 13:28:57 -0300

    feat: Added /new-transaction/:type and /transactions Endpoints

--- src/app.js
+++ src/app.js
@@ -5,6 +5,7 @@ import cors from "cors";
 import dotenv from "dotenv";
 import joi from "joi";
 import bcrypt from "bcrypt";
+import dayjs from "dayjs";
 import { v4 as uuid } from "uuid";
 
 dotenv.config();
@@ -24,17 +25,24 @@ const db = mongoClient.db();
 
 //Schemas:
 const registerSchema = joi.object({
-  name: joi.string().min(3).required(),
+  name: joi.string().required(),
   email: joi.string().email().trim().required(),
   password: joi.string().min(3).required(),
 });
 
 const loginSchema = joi.object({
   email: joi.string().email().trim().required(),
-  password: joi.string().min(3).required(),
+  password: joi.string().required(),
 });
 
-app.post("/cadastro", async (req, res) => {
+const transactionSchema = joi.object({
+  type: joi.string().allow("entrada", "saida").only().required(),
+  token: joi.string().min(36).max(36).required(),
+  description: joi.string().min(4).required(),
+  amount: joi.number().min(0.01).required(),
+});
+
+app.post("/register", async (req, res) => {
   const { name, email, password } = req.body;
 
   const validation = registerSchema.validate({ name, email, password }, { abortEarly: false });
@@ -50,7 +58,7 @@ app.post("/cadastro", async (req, res) => {
     password: stripHtml(password).result.trim(),
   };
 
-  cleanRegister.password = bcrypt.hashSync(password, 1);
+  cleanRegister.password = bcrypt.hashSync(password, 10);
 
   try {
     const validUser = await db.collection("users").findOne({ email: cleanRegister.email });
@@ -63,7 +71,7 @@ app.post("/cadastro", async (req, res) => {
   }
 });
 
-app.post("/", async (req, res) => {
+app.post("/login", async (req, res) => {
   const { email, password } = req.body;
 
   const validation = loginSchema.validate({ email, password }, { abortEarly: false });
@@ -85,12 +93,69 @@ app.post("/", async (req, res) => {
     const passwordValidation = bcrypt.compareSync(cleanLogin.password, user.password);
     if (!passwordValidation) return res.status(401).send("Wrong password");
 
+    await db.collection("sessions").deleteOne({ id: user._id });
     const token = uuid();
-    await db.collection("section").insertOne({ name: user.name, email: user.email, id: user._id, token });
+    await db.collection("sessions").insertOne({ id: user._id, token });
 
     res.status(200).send({ name: user.name, token });
   } catch (err) {
     res.status(500).send(err.message);
+  }
+});
+
+app.post("/new-transaction/:type", async (req, res) => {
+  const { authorization } = req.headers;
+  const { description, amount } = req.body;
+  const { type } = req.params;
+  const token = authorization?.replace("Bearer ", "");
+
+  if (!token) return res.status(401).send("Invalid token!\nPlease login again!");
+
+  const validation = transactionSchema.validate({ type, token, description, amount }, { abortEarly: false });
+
+  if (validation.error) {
+    const errors = validation.error.details.map((detail) => detail.message);
+    return res.status(422).send(errors);
+  }
+
+  const cleanTransaction = {
+    type: type,
+    token: stripHtml(token).result.trim(),
+    description: stripHtml(description).result.trim(),
+    amount: amount,
+  };
+
+  try {
+    const user = await db.collection("sessions").findOne({ token: cleanTransaction.token });
+    if (!user) return res.status(401).send("Invalid token!\nPlease login again!");
+
+    const newTransaction = await db.collection("transactions").insertOne({
+      id: user.id,
+      type: cleanTransaction.type,
+      description: cleanTransaction.description,
+      date: dayjs().toDate(),
+      amount: parseFloat(amount.toFixed(2)),
+    });
+    if (newTransaction) res.sendStatus(201);
+  } catch (err) {
+    return res.status(500).send(err.message);
+  }
+});
+
+app.get("/transactions", async (req, res) => {
+  const { authorization } = req.headers;
+
+  const token = authorization?.replace("Bearer ", "");
+  if (!token) return res.status(401).send("Invalid token!\nPlease login again!");
+
+  try {
+    const user = await db.collection("sessions").findOne({ token: token });
+    if (!user) return res.status(401).send("Invalid token!\nPlease login again!");
+
+    const transactions = await db.collection("transactions").find({ id: user.id }).sort({ date: -1 }).toArray();
+    return res.send(transactions);
+  } catch (e) {
+    return res.status(500).send(e.message);
   }
 });
 
