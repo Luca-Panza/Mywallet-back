6f1c90fff4c6c9688521dffe55cef92216057836
Luca-Panza <luca.panza.007@gmail.com>
2023-12-10 17:59:49 -0300

    chore: add .env.example file and assets directory

--- src/app.js
+++ src/app.js
@@ -7,6 +7,9 @@ app.use(cors());
 app.use(json());
 
 app.use(indexRouter);
+app.get("/health", (req, res) => {
+  res.send("OK!");
+});
 
 const port = process.env.PORT || 5000;
 app.listen(port, () => console.log(`Port:${port}/`));
